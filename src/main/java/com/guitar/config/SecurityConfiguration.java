a1d3bbc5d5c40541fc8b1068337b94554ac0a54e
kamlendra <kamlendra.chauhan@gmail.com>
2016-09-24 18:38:13 +0530

    authentication, authorization

--- src/main/java/com/guitar/config/SecurityConfiguration.java
+++ src/main/java/com/guitar/config/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.guitar.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,4 +13,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("sa").password("sa").roles("USER").and().withUser("admin")
+				.password("admin").roles("USER", "ADMIN");
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.csrf().disable();
+	}
 }
