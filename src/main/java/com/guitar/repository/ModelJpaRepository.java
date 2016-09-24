9dea93ef0a8ecd770f26716ccc30b94819e5e672
kamlendra <kamlendra.chauhan@gmail.com>
2016-09-24 12:56:58 +0530

    projections, security

--- src/main/java/com/guitar/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/repository/ModelJpaRepository.java
@@ -8,21 +8,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.model.Model;
+import com.guitar.model.projections.ModelDetailView;
 
 @Repository
+@RepositoryRestResource(excerptProjection = ModelDetailView.class)
 public interface ModelJpaRepository extends JpaRepository<Model, Long>, ModelJpaRepositoryCustom {
 	List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal low, BigDecimal high);
-	
+
 	List<Model> findByModelTypeNameIn(List<String> types);
-	
+
 	@Query("select m from Model m where m.price >= :lowest and m.price <= :highest and m.woodType like :wood")
-	Page<Model> queryByPriceRangeAndWoodType(@Param("lowest") BigDecimal lowest,
-											 @Param("highest") BigDecimal high,
-											 @Param("wood") String wood,
-											 Pageable page);
-	
+	Page<Model> queryByPriceRangeAndWoodType(@Param("lowest") BigDecimal lowest, @Param("highest") BigDecimal high,
+			@Param("wood") String wood, Pageable page);
+
 	List<Model> findAllModelsByType(@Param("name") String name);
 }
